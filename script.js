d83f4f7e99a7a0ef1e3cca30d6881e4d84b02011
SaiLokhande29 <sailokhande04@gmail.com>
2025-02-09 00:13:32 +0530

    25

--- script.js
+++ script.js
@@ -37,12 +37,45 @@ $(document).ready(function () {
   });
 });
 
+// Instagram Carousel Functionality
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("carousel-item");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function moveSlide(n) {
+    slideIndex += n;
+    if (slideIndex > document.getElementsByClassName("carousel-item").length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = document.getElementsByClassName("carousel-item").length}
+    showSlidesManually();
+}
+
+function showSlidesManually() {
+    let i;
+    let slides = document.getElementsByClassName("carousel-item");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
+
 AOS.init({
   duration: 1000,
   once: true,
 });
-
-
 
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
@@ -58,13 +91,7 @@ function reveal() {
     }
   }
 }
-let machineContainer = document.querySelector('.machine-container');
-function autoScroll() {
-    machineContainer.scrollBy({
-        left: 200,
-        behavior: 'smooth'
-    });
-}
+
 // Set an interval to auto-scroll every 3 seconds
 setInterval(autoScroll, 3000);
 window.addEventListener("scroll", reveal);
