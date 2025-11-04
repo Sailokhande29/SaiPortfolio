1b61be751f617f301e1857d63c833187de6703bb
Sai Lokhande <sailokhande04@gmail.com>
2025-11-04 23:10:55 +0530

    Edit4

--- script.js
+++ script.js
@@ -1,76 +1,80 @@
 var type = new Typed('.typing-text', {
-  strings: ['Student','Maker', 'Innovator','Programmer','Designer','Thinker'],
-  typeSpeed: 120,
-  loop: true
+    strings: ['Student', 'Maker', 'Innovator', 'Programmer', 'Designer', 'Thinker'],
+    typeSpeed: 120,
+    loop: true
 });
 
-$(document).ready(function () {
-  $('#menu').click(function () {
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
-  });
-  $(window).on('scroll load', function () {
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
+$(document).ready(function() {
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
 
-    if($(window).scrollTop() > 0){
-      $('.top').show();
-    }else{
-      $('.top').hide();
-    }
-  });
-  
-  //smooth scroll
-  $('a[href*="#"]').on('click',function(e){
+    $(window).on('scroll load', function() {
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
 
-    e.preventDefault();
+        if ($(window).scrollTop() > 0) {
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+    });
 
-    $('html, body').animate({
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
 
-      scrollTop : $($(this).attr('href')).offset().top,
-
-    },
-      1000, 
-      'linear'
-    );
-
-  });
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        }, 1000, 'linear');
+    });
 });
 
 AOS.init({
-  duration: 1000,
-  once: true,
+    duration: 1000,
+    once: true,
 });
 
-
-
 function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 0;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("anim");
-    }
-    else {
-      reveals[i].classList.remove("anim");
-    }
-  }
-}
-function moveSlide(carouselId, step) {
-  const track = document.getElementById(carouselId);
-  const totalItems = track.children.length;
-  let index = parseInt(track.getAttribute('data-index')) || 0;
+    var reveals = document.querySelectorAll(".reveal");
 
-  index += step;
-  if (index < 0) index = totalItems - 1;
-  if (index >= totalItems) index = 0;
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 0;
 
-  track.style.transform = `translateX(-${index * 100}%)`;
-  track.setAttribute('data-index', index);
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("anim");
+        } else {
+            reveals[i].classList.remove("anim");
+        }
+    }
 }
-// Set an interval to auto-scroll every 3 seconds
-setInterval(autoScroll, 3000);
+
 window.addEventListener("scroll", reveal);
 reveal();
+
+function sendMail() {
+    (function() {
+        emailjs.init("ZMftIU4zwjSjXXAff");
+    })();
+
+    var params = {
+        sendername: document.querySelector("#sendername").value,
+        phone: document.querySelector("#phone").value,
+        subject: document.querySelector("#subject").value,
+        replyto: document.querySelector("#reply_to").value,
+        message: document.querySelector("#message").value,
+    };
+
+    var serviceID = "service_n2qdpdd";
+    var templateID = "template_mos01mx";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            alert("Email Sent Successfully!");
+        })
+        .catch(() => {
+            alert("Error sending email!");
+        });
+}
